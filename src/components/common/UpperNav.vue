ef296b322eb2edfd482e45ca71678dfab757535f
Andre Ravazzi <amravazzi@gmail.com>
2020-04-19 01:46:19 -0300

    ref(*): update naming

--- src/components/common/UpperNav.vue
+++ src/components/common/UpperNav.vue
@@ -106,7 +106,7 @@ export default {
       }
     })
 
-    document.title = 'Altec Relatórios'
+    document.title = 'Your Company Relatórios'
   },
   computed: {
     showReportFilter: function () {
